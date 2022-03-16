f56a51e4ddaf05a8a54078b02a24d1f6dc54bd06
Ben Manley <benjamintmanley@gmail.com>
2022-03-16 12:59:52 -0500

    add class details API call

--- src/pages/ClassList/ClassList.jsx
+++ src/pages/ClassList/ClassList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { getClassList } from '../../services/api-calls'
+import { Link } from 'react-router-dom'
 
 const ClassList = (props) => {
   const [classes, setClasses] = useState([])
@@ -7,7 +8,7 @@ const ClassList = (props) => {
   useEffect(()=> {
     getClassList()
     .then(classData => setClasses(classData.results))
-  })
+  }, [])
 
   return (
     <>
@@ -15,14 +16,16 @@ const ClassList = (props) => {
         <h3>Class List</h3>
         <div className='icon-container'>
           {classes.map((classTitle) => (
-            <div id="classDiv" key={classTitle.index}>
-              <img 
-                style={{ width: "100px", height: "100px" }}
-                src={`/images/${classTitle.name}.svg`} 
-                alt="class-logo"
-              />
-              {classTitle.name}
-            </div>
+            <Link to='/class' state={{classTitle}} key={classTitle.index}>
+              <div id="classDiv" >
+                <img 
+                  style={{ width: "100px", height: "100px" }}
+                  src={`/images/${classTitle.name}.svg`} 
+                  alt="class-logo"
+                />
+                {classTitle.name}
+              </div>
+            </Link>
           ))}
         </div>
       </div>
--- src/services/api-calls.js
+++ src/services/api-calls.js
@@ -4,3 +4,9 @@ export function getClassList() {
   return fetch(`${baseUrl}/api/classes`)
   .then(res => res.json())
 }
+
+// fetch to https://www.dnd5eapi.co/api/classes/whateverClass
+export function getDetails(apiUrl) {
+  return fetch(`${baseUrl}${apiUrl}`)
+  .then(res => res.json())
+}
