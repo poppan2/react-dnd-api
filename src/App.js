f5684586c8634742017895bca65a7c2c9645f058
Ben Manley <benjamintmanley@gmail.com>
2022-03-16 09:38:35 -0500

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+import NavBar from './components/NavBar/NavBar';
+
 import './App.css';
 
 function App() {
+  const [navItems, setNavItems] = useState([
+    {url: "/class-list", name: "Class List"}, 
+    {url: "/monster-list", name: "Scary Monsters"}, 
+    {url: "/spell-search", name: "Search for Spells"}
+  ])
   return (
     <>
-      <h1>This is our app!</h1>
+      <NavBar navItems={navItems} />
     </>
   );
 }
